918ef85e923174c95c3f821b47ac98409ec583e3
Madeline Savoiu <savoiu@csu.fullerton.edu>
2024-02-25 14:17:45 -0800

    New HTML templates for activity pages, started CSS style sheets.

--- app.py
+++ app.py
@@ -12,9 +12,20 @@ def home():
 def breathing():
     return render_template('breathing.html')
 
-@app.route('/leaves-on-a-stream')
+@app.route('/getting-leaves')
 def leaves():
-    return render_template('leaves.html')
+    return render_template('get_leaves.html')
+
+@app.route('/getting-leaves', methods=['POST'])
+def leavesPost():
+    leaves = request.form['leaf'] #comma separated string 'list'
+    leaves_list = leaves.split(', ') # separating items into a real list
+    return render_template('leaves.html', leaves = leaves_list)
+
+@app.route('/leaves-on-a-stream')
+def gettingLeaves():
+    leaf_text = input("Enter your cause of stress: ")
+    return render_template('leaves.html', leaf = leaf_text)
 
 @app.route('/drawing')
 def drawing():
@@ -22,8 +33,8 @@ def drawing():
 
 @app.route('/drawing-prompt')
 def drawingPrompt():
-    prompts_list = ["Something or someone that brings you comfort", "Something or someone that you are grateful for", "Different kinds of flowers", "Different kinds of animals", "Swiggles and ripples (ex. drop in water)", "Fanart of your favorite media", "Design clothing that you would want to wear", "The feeling of happiness", "The feeling of sadness", "The feeling of loneliness", "The feeling of love",  "The feeling of loneliness",  "The feeling of anger",  "The feeling of grief",  "The feeling of disgust",  "The feeling of jealously",  "The feeling of excitement",  "The feeling of anxiety",  "The feeling of empowerment", "The feeling of acceptance", "Your brave space (an enviroment where you feel brave enough to express yourself)", "Family or friends", "Pets", "Favorite place", "Favorite food", "Favorite color"]
-    index = random.randint(0, 26)
+    prompts_list = ["Something or someone that brings you comfort", "Something or someone that you are grateful for", "Different kinds of flowers", "Different kinds of animals", "Swiggles and ripples (ex. drop in water)", "Fanart of your favorite media", "Design clothing that you would want to wear", "The feeling of happiness", "The feeling of sadness", "The feeling of loneliness", "The feeling of love",  "The feeling of loneliness",  "The feeling of anger",  "The feeling of grief",  "The feeling of disgust",  "The feeling of jealousy",  "The feeling of excitement",  "The feeling of anxiety",  "The feeling of empowerment", "The feeling of acceptance", "Your brave space (an enviroment where you feel brave enough to express yourself)", "Family or friends", "Pets", "Favorite place", "Favorite food", "Favorite color"]
+    index = random.randint(0, 25)
     random_prompt = prompts_list[index]
 
     return render_template('drawing_prompt.html', prompt = random_prompt)
