056d696b9bf8ca45b640b020315c53452b404a8d
Brock Nguyen <N1NANGUYEN@csu.fullerton.edu>
2024-02-24 21:54:58 -0800

    Turtle drawing a circle for the breathing exercise page.

---
+++ Animate.py
@@ -0,0 +1,29 @@
+import turtle
+import time
+from turtle import *
+
+juno = turtle.Turtle()
+myriad = turtle.Turtle()
+# timer_text = turtle.Turtle()
+
+juno.shapesize(1)
+juno.pensize(5)
+turtle.delay(15)
+juno.color("#0A970C")
+juno.shape("turtle")
+juno.begin_fill()
+juno.circle(150)
+juno.end_fill()
+juno.color("#BFFFED")
+juno.begin_fill()
+juno.circle(150)
+juno.end_fill()
+juno.color("#0A970C")
+juno.begin_fill()
+juno.circle(150)
+juno.end_fill()
+juno.color("#BFFFED")
+juno.begin_fill()
+juno.circle(150)
+juno.end_fill()
+turtle.done()
